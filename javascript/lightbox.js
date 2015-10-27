a92d76eb68f8d44dff929a6450c758d18aec87b6
Oly Su <oly@silverstripe.com>
2015-10-27 15:28:06 +1300

    Added Android workaround to use position absolute instead of fixed

--- javascript/lightbox.js
+++ javascript/lightbox.js
@@ -5,7 +5,11 @@
  */
 
 (function($, document, window) {
-	// Html for the lightbox, may move this out to somewhere else later.
+
+	// Common selectors
+	var $html = $('html');
+
+	// Modal mark-up, may move this to somewhere else laster
 	var modal = $([
 		'<div class="lightbox-container" style="display:none;">',
 			'<div class="lightbox-dialog">',
@@ -16,11 +20,38 @@
 		'</div>'].join('')
 	).appendTo('body');
 
+
+	/**
+	 * Device detection
+	 * ================
+	 *
+	 * Mainly to target devices on Android OS, as it does not support `position: fixed;` very well.
+	 *
+ 	 */
+	var ua = navigator.userAgent.toLowerCase(),
+		isAndroid = ua.indexOf('android'),
+		screenPos;
+
+	// Android workarounds
+	if (isAndroid) {
+		$html.addClass('android'); // Add class to allow Android specific styling
+	}
+
+
+	// Bind click events
 	$(document)
 		.on('click', 'a.lightbox', function (e) {
 			var href = $(this).attr('href'),
 				content = modal.find('.lightbox-content').html('');
 			modal.show().addClass('lightbox-loading');
+
+			$html.addClass('lightbox');
+			modal.show().addClass('lightbox-loading');
+
+			if (isAndroid) {
+				screenPos = $(window).scrollTop(); // Get current screen position
+				window.location = '#page-top'; // Anchor to top of the `position: absolute;` Android lightbox
+			}
 
 			// get the content linked and inject it to the content of the lightbox.
 			$.ajax({
@@ -35,12 +66,22 @@
 					content.html(textStatus);
 				}
 			});
+
 			// stop the link from following the link
 			e.preventDefault();
 		})
 		.on('click', '.lightbox-close-btn, .lightbox-overlay', function (e) {
+
 			// hide the modal
 			modal.hide();
+			$html.removeClass('lightbox');
+
+			if (isAndroid) {
+				// Scroll back to screen position before lightbox is opened
+				$('html, body').scrollTop(screenPos);
+			}
+
+			e.preventDefault();
 		});
 
 
